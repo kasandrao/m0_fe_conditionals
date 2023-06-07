098c277e1d497ec5aef462436eec83927435fb1b
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-07 12:20:10 -0600

    Add part 3

--- comparisons.js
+++ comparisons.js
@@ -68,10 +68,11 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 // YOU DO:
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 // YOU DO:
 // Determine if the dog loves to play and is a puppy
 // HINT: Use the age variable and assume that a puppy is less than 2 years old
+console.log(lovesToPlay && age < 2);
